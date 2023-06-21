0bf5c945f8806e3f143d3c97e8f91a41d3eff3d6
Mary <mary.pommer@tptlive.ee>
2023-06-21 20:56:50 +0300

    add desktop sidebar image

--- src/App.js
+++ src/App.js
@@ -1,11 +1,57 @@
 import logo from './logo.svg';
 import './App.css';
+import { Card, CardImg, CardImgOverlay, CardText, CardTitle, Col, Container, Row } from 'reactstrap';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      <main className="App-header">
+        <Container>
+          <Row>
+            <Col>
+              <Card inverse>
+                <CardImg
+                  alt="Card image cap"
+                  src="./images/bg-sidebar-desktop.svg"
+                  style={{
+                    height: 700
+                  }}
+                  width="100%"
+                />
+                <CardImgOverlay>
+                  <CardTitle tag="h5">
+                    Step 1
+                  </CardTitle>
+                  <CardText>
+                    Your info
+                  </CardText>
+                  <CardTitle tag="h5">
+                    Step 2
+                  </CardTitle>
+                  <CardText>
+                    Select plan
+                  </CardText>
+                  <CardTitle tag="h5">
+                    Step 3
+                  </CardTitle>
+                  <CardText>
+                    Add-ons
+                  </CardText>
+                  <CardTitle tag="h5">
+                    Step 4
+                  </CardTitle>
+                  <CardText>
+                    Summary
+                  </CardText>
+                </CardImgOverlay>
+              </Card>
+            </Col>
+            <Col>Col 2
+            </Col>
+          </Row>
+        </Container>
+        {/*<img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -16,8 +62,8 @@ function App() {
           rel="noopener noreferrer"
         >
           Learn React
-        </a>
-      </header>
+        </a>*/}
+      </main>
     </div>
   );
 }
