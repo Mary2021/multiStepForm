a5de313d5821bd718a1c4ecdd8185e2fc444af72
Mary <mary.pommer@tptlive.ee>
2023-10-25 18:17:29 +0300

    delete Steps.test.js and ci.yml

--- src/Steps.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import Steps from './components/Steps';
-
-test('renders img with alt Card image cap', () => {
-  render(<Steps />)
-  const cardImg = screen.getByAltText(/Card image cap/i)
-  expect(cardImg).toBeInTheDocument();
-});
